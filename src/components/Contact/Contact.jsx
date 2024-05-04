e03c09d220e374b58d63423473ce9c024fb765f0
With0utN3me <144373584+With0utN3me@users.noreply.github.com>
2024-05-04 18:25:12 +0200

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import css from "./Contact.module.css";
+import { useDispatch } from 'react-redux';
+import { deleteContact } from "../../redux/contactsOps.js";
+const Contact = ({ name, number, id }) => {
+    const dispatch = useDispatch();
+    const handleDelete = () => dispatch(deleteContact(id));
+    return(
+        <div className={css["contact-wrapper"]}>
+            <div className={css["contact-info"]}>
+                <p className={css["contact-p"]}>&#128222;{name}</p>
+                <p className={css["contact-p"]}>&#128100;{number}</p>
+            </div>
+            <button className={css["contact-button"]} onClick={handleDelete}>Delete</button>
+        </div>
+    )
+}
+export default Contact;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,22 @@
+import css from "./SearchBox.module.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { changeFilter } from "../../redux/filtersSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+const SearchBox = () => {
+    const dispatch = useDispatch();
+    const nameFilter = useSelector(selectNameFilter);
+    const handleFilterChange = (filter) => dispatch(changeFilter(filter));
+    return(
+        <div>
+            <p className={css["search-p"]}>Find contacts by name&#128270;</p>
+            <input 
+                className={css["search-input"]}
+                type="text"
+                value={nameFilter}
+                onChange={(e) => handleFilterChange(e.target.value)}
+            />
+        </div>
+    )
+}
+export default SearchBox;
